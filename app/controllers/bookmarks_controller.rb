f91003af94d215ad043fd6f7fffde67b28d15a19
Kristina Sananikone <snk.kristina@gmail.com>
2021-06-10 16:46:34 +0200

    Controllers and routes done. Views done too. Just need to add the bookmarks

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,38 @@
+class BookmarksController < ApplicationController
+  def index
+    @bookmarks = Bookmark.all
+  end
+
+  def show
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
+
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # resources :movies
+  resources :lists, only: %i[index new show create] do
+    resources :bookmarks, only: %i[new create]
+  end
+  resources :bookmarks, only: :destroy
 end
